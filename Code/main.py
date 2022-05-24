763d3e4c1821c78282ef3766e184bd67b59b342b
TheRealDulanOoga <89281817+TheRealDulanOoga@users.noreply.github.com>
2022-05-24 17:04:12 -0400

    Added functionality for DiscordComponents

--- Code/main.py
+++ Code/main.py
@@ -1,15 +1,12 @@
 import discord
 from discord.ext import commands
-import os
+from discord_components import ComponentsBot
 
 from help_cog import help_cog
 from music_cog import music_cog
 
-bot = commands.Bot(command_prefix='!')
+bot = ComponentsBot(command_prefix='!')
 
-bot.remove_command("help")
-
-# bot.add_cog(help_cog(bot))
 bot.add_cog(music_cog(bot))
 
 with open('token.txt', 'r') as file:
