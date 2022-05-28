22c8edf09fb1161420f34c094a5ab9767be8a7ac
TheRealDulanOoga <89281817+TheRealDulanOoga@users.noreply.github.com>
2022-05-28 09:11:34 -0400

    Added full basic functionality for help commands

--- Code/help_cog.py
+++ Code/help_cog.py
@@ -1,39 +1,94 @@
 import discord
+import music_cog
 from discord.ext import commands
 
 
 class help_cog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
-        self.initialMessage = """
-`Hello there! I'm now online.
-Type "!help" for general commands`
-"""
-        self.helpMessage = """
-`General Commands:
-!play - 
-!resume - 
-!pause - 
-!queue -
-!clear - 
-!leave - 
-!help - `
-"""
-        self.textChannelText = []
+        self.embedOrange = 0xeab148
 
     @commands.Cog.listener()
     async def on_ready(self):
+        sendToChannels = []
         for guild in self.bot.guilds:
-            for channel in guild.text_channels:
-                self.textChannelText.append(channel)
+            channel = guild.text_channels[0]
+            sendToChannels.append(channel)
+        helloEmbed = discord.Embed(
+            title="Hello There!",
+            description="""
+            Hello, I'm Bobbert! You can type any command after typing my prefix **`'!'`** to activate them. Use **`!help`** to see some command options.
+            
+            Here is a link to my [source code](https://github.com/TheRealDulanOoga/Bobbert.git) if you wanted to check it out!""",
+            colour=self.embedOrange
+        )
+        for channel in sendToChannels:
+            await channel.send(embed=helloEmbed)
 
-        await self.send_to_all(self.initialMessage)
+    @commands.command(
+        name="help",
+        aliases=["h"],
+        help="""
+            (command_name)
+            Provides a description of all specified commands
+            Gives a description of a specified command (optional). If no command is specified, then gives a less detailed description of all commands.
+            """
+    )
+    async def help(self, ctx, arg=""):
+        music = self.bot.get_cog('music_cog')
+        commands = music.get_commands()
+        command = None
+        if arg != "":
+            for i, c in enumerate(commands):
+                if c.name == arg:
+                    command = commands[i]
+            if command == None:
+                await ctx.send("That is not a name of an available command.")
+                return
 
-    async def send_to_all(self, msg):
-        for textChannel in self.textChannelText:
-            await textChannel.send(msg)
+            arguments = command.help.split("\n")[0]
+            longHelp = command.help.split("\n")[2]
+            aliases = ""
+            for a in command.aliases:
+                aliases += f"!{a}, "
+            aliases = aliases.rstrip(", ")
+            commandsEmbed = discord.Embed(
+                title=f"!{command.name} Command Info",
+                description=f"""
+                Arguments: **`{arguments}`**
+                {longHelp}
 
-    @commands.command(name="help", help="Displays all the available commands")
-    async def help(self, ctx):
-        await ctx.send(self.helpMessage)
+                Aliases: **`{aliases}`**       
+                """,
+                colour=self.embedOrange
+            )
+
+        else:
+            commandDescription = "**`!help (command)`** - Provides a description of all commands or a longer description of an inputted command\n\n"
+            for c in commands:
+                arguments = c.help.split("\n")[0]
+                shortHelp = c.help.split("\n")[1]
+                commandDescription += f"**`!{c.name} {arguments}`** - {shortHelp}\n"
+            commandsEmbed = discord.Embed(
+                title="Command List",
+                description=commandDescription,
+                colour=self.embedOrange
+            )
+
+        commandKey = """
+            **`Command Prefix`** - '!'
+
+            **`!command <>`** - No arguments required
+            **`!command ()`** - Optional argument
+            **`!command []`** - Required argument
+            **`!command [arg]`** - 'arg' specifies argument type (eg. "url" or "keywords")
+            **`!command (this || that)`** - Options between mutually exclusive inputs (this or that)
+        """
+
+        keyEmbed = discord.Embed(
+            title="Key",
+            description=commandKey,
+            colour=self.embedOrange
+        )
+        await ctx.send(embed=commandsEmbed)
+        await ctx.send(embed=keyEmbed)
