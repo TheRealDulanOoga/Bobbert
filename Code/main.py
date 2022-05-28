62e494ad5142bb0d875857c85fbd288bbfa10e4d
TheRealDulanOoga <89281817+TheRealDulanOoga@users.noreply.github.com>
2022-05-28 09:11:10 -0400

    Added !help status and help cog

--- Code/main.py
+++ Code/main.py
@@ -1,11 +1,16 @@
 from discord_components import ComponentsBot
+from discord import Activity, ActivityType
 
 from help_cog import help_cog
 from music_cog import music_cog
 
-bot = ComponentsBot(command_prefix='!')
+activity = Activity(name="!help", type=ActivityType.listening)
+bot = ComponentsBot(command_prefix='!', activity=activity)
+
+bot.remove_command('help')
 
 bot.add_cog(music_cog(bot))
+bot.add_cog(help_cog(bot))
 
 with open('token.txt', 'r') as file:
     token = file.read()
